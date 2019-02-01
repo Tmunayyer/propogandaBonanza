10bbeefbace1cb638d113336c3c96f389d59b4ac
Thomas Munayyer <tmunayyer@gmail.com>
2019-02-01 13:58:42 -0500

    hooked up to microsoft azure

---
+++ utilities/microsoftAzure.js
@@ -0,0 +1,40 @@
+const https = require('https');
+
+const apiKey = process.env.APIKEY;
+const uri = 'eastus.api.cognitive.microsoft.com';
+const path = '/text/analytics/v2.0/sentiment';
+
+const request_params = {
+  method: 'POST',
+  hostname: uri,
+  path: path,
+  headers: {
+    'Ocp-Apim-Subscription-Key': apiKey
+  },
+  json: true
+};
+
+const AzureData = (response, cb) => {
+  let body = '';
+  response.on('data', (d) => {
+    body += d;
+  });
+  response.on('end', () => {
+    cb(JSON.parse(body));
+  });
+  response.on('error', (e) => {
+    console.log('ERROR:' + e.message);
+  });
+};
+
+const getAnalysis = (documents, cb) => {
+  let body = JSON.stringify(documents);
+
+  let req = https.request(request_params, (azure_response) => {
+    AzureData(azure_response, cb);
+  });
+  req.write(body);
+  req.end();
+};
+
+module.exports = getAnalysis;
