c39cd8f4ef00d0db6e19d3d417f37852c3b73747
Thomas Munayyer <tmunayyer@gmail.com>
2019-02-01 14:42:19 -0500

    webpack, react, rendering to page

---
+++ client/src/components/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <p>Hello from React</p>;
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  mode: 'development',
+  entry: './client/src/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  output: {
+    path: __dirname + '/client/dist',
+    filename: 'bundle.js'
+  }
+};
